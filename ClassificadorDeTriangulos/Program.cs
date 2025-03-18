4330a589dbb67956d6fd73d818ab7bf8786b2c91
estevaosantosribeiro <estevaosantosribeiro@gmail.com>
2025-03-18 15:39:44 -0300

    Add project files.

---
+++ ClassificadorDeTriangulos/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace ClassificadorDeTriangulos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                // exibir o cabecalho
+                Console.Clear();
+                Console.WriteLine("---------------------------------------------");
+                Console.WriteLine("Classificador de Triângulos");
+                Console.WriteLine("---------------------------------------------");
+
+                // receber a entrada (input) do usuário
+                Console.Write("Informe o valor do lado X: ");
+                int ladoX = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Informe o valor do lado Y: ");
+                int ladoY = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Informe o valor do lado Z: ");
+                int ladoZ = Convert.ToInt32(Console.ReadLine());
+
+                // exibir  os valores digitados
+                Console.WriteLine("---------------------------------------------");
+                Console.WriteLine("Valores informados: ");
+                Console.WriteLine("Lado X: " + ladoX);
+                Console.WriteLine("Lado Y: " + ladoY);
+                Console.WriteLine("Lado Z: " + ladoZ);
+
+                Console.Write("Deseja continuar? (S/N) ");
+                string opcaoContinuar = Console.ReadLine()!.ToUpper();
+
+                if (opcaoContinuar != "S")
+                    break;
+            }
+        }
+    }
+}
